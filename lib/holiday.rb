a1dd133168c04a01ccdce0e389ee32856def64df
Vanessa Toro <vanetoro84@gmail.com>
2018-03-26 23:33:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -110,7 +110,6 @@ def all_holidays_with_bbq(holiday_hash)
     holidays = []
       holiday_hash.each do |season, data_hash|
         data_hash.each do |event, supplies|
-          
           i = 0
           while i < supplies.length
               if supplies[i] == 'BBQ'
